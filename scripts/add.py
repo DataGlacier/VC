4fec7452e79793339e8691fd63d8088259ebd345
Data Glacier <76258539+DataGlacier@users.noreply.github.com>
2020-12-23 22:16:51 +0530

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -1,1 +1,34 @@
+import json, os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# User can add name and favourite sport in response.json
+# default sport Cricket will be added incase user does not provide fav sport
 
+
+def load_json():
+  
+  with open('../response.json') as json_obj:
+    response = json.load(json_obj)
+  return response
+
+response = loan_json()
+
+
+
+
+
+
+def write_sport():
+  name = input("Please add your name: ")
+  sport = input("Please add your favourite sports name: ")
+  if(sport ==""):
+    sport = 'Cricket'
+  if(name):
+    response[name] = sport
+    write_json(response)
+    
+    
+if __name__ == "__main__":
+  call_sport()
+
+  
+call_sport()
