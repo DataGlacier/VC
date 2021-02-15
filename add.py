b5c6a7826d263bebbe38ba3c29aad80923d514ad
irem06 <50293205+irot06@users.noreply.github.com>
2021-02-15 18:25:06 +0300

    add name and fav sport

---
+++ add.py
@@ -0,0 +1,7 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /Users/irem/Documents/VC/scripts/add.py ===============
+Please add your name: Irem Tanriverdi
+Please add your favourite sports name: Tennis
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,2 @@
 
+irem tanriverdi
