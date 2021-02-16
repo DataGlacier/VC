f3b2d222a648aaa5d984f223432fe9b60e0b203b
dangerous2 <chepkorirberil97@gmail.com>
2021-02-16 23:25:42 +0300

    making a change in a branch

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name:Beril ")
+    sport = input("Please add your favourite sports name:Athletics ")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
