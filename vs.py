2e5286787df7d83809d5f8cac0b5ced1528d766e
Phoebe <ph.gathuku@gmail.com>
2021-02-17 09:53:45 +0000

    created vs.py to replace add2.py

---
+++ vs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 09:42:27 2021
+
+@author: user
+"""
+#vs.py
+name = input("Enter your name : ")
+print("Hello", name)
+
+sport = input("Enter favorite sport : ")
+print(sport)
