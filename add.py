b4e36fed7320e7c7da7f6e1117ca72c07c9666f2
Phoebe <ph.gathuku@gmail.com>
2021-02-17 08:17:50 +0000

    adding add2.py

---
+++ add.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 17:01:02 2021
+
+@author: user
+"""
+#add.py
+def my_name() :
+    return 'My name is Phoebe'
+
+my_name() 
+
+def fav_sport() :
+    return 'and my favorite sport is volleyball.'
+
+fav_sport()
+
+
+
+
---
+++ add2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 08:08:15 2021
+
+@author: user
+"""
+
+#add2.py
+name = input("Enter your name : ")
+print("Hello", name)
+
+sport = input("Enter favorite sport : ")
+print(sport)
