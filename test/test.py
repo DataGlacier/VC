013ad3d85d1609dbd366712222267a7d9aaf6fe1
kevigocha2 <75636395+kevigocha2@users.noreply.github.com>
2021-02-18 12:28:12 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,5 +8,3 @@ def test_load_json():
   response = load_json()
   assert isinstance(response, dict)
   assert response.get("Data Glacier") =="Cricket"
-# Name and fav sport added to the program and witouth errors
-# Kevin, Basket
