db4eaf73a35ebcdc0ea15770c0cd0f4b70550192
dokdok33 <ozandokuyucu1@gmail.com>
2021-02-20 09:04:55 +0300

    Week_1

--- scripts/add.py
+++ scripts/add.py
@@ -35,12 +35,3 @@ if __name__ == "__main__":
 
 call_sport()
 
-import sys,os
-sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-from scripts.add import load_json
-
-
-def test_load_json():
-  response = load_json()
-  assert isinstance(response, dict)
-  assert response.get("Data Glacier") =="Cricket"
