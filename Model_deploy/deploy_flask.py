f99462624fce6666fab112fd6e197dc3c0d5902b
rodbergerrone <rodbergerrone@icloud.com>
2021-03-20 23:44:46 +0100

    20.03.2021

--- Model_deploy/deploy_flask.py
+++ Model_deploy/deploy_flask.py
@@ -4,24 +4,26 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('pickle_model', 'rb'))
+with open('pickle_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction.model.predict(final_features)
+    prediction = model.predict(final_features)
     
-    output = round(prediciont[0], 2)
+    prediction = round(prediction[0][0], 2)
     
-    return render_template('index.html', prediction_text='CO2 emission for this size of engine should be {}'. format(output))
+    return render_template('index.html', prediction_text='CO2 emission for this size of engine should be {} G/KM'
+                           .format(prediction))
 
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
-    
