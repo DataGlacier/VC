e1ba91221bcac5e32d19869afafa6ebaf1e30a22
rodbergerrone <rodbergerrone@icloud.com>
2021-03-28 17:20:57 +0200

    28.03.2021

--- Model_deploy/deploy_flask.py
+++ Model_deploy/deploy_flask.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, make_response
 import pickle
 
 
@@ -23,6 +23,22 @@ def predict():
     
     return render_template('index.html', prediction_text='CO2 emission for this size of engine should be {} G/KM'
                            .format(prediction))
+
+@app.route('/api')
+@app.route('/api/')
+@app.route('/api/<l>')
+@app.route('/api/<l>/')
+def api_l(l=None):
+    if not l:
+        message = jsonify(message='Size of engine (l) not provided')
+        return make_response(message, 400)
+    else:
+        l = float(l)
+        prediction = model.predict(np.array([[l]]))
+        prediction = round(prediction[0][0], 2)
+        return jsonify(
+            CO2_emission=prediction,
+            engine_size=l)
 
 
 if __name__ == "__main__":
