b57f2aa5d30b8378a86d0ab0708489ebc862a0f6
IgorQueiroz23031988 <igor.queiroz2303@gmail.com>
2021-07-13 21:58:57 +0100

    Full Name and Favorite Sport added.

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Igor A. de Queiroz")
-    sport = input("Football")
+    name = input("Please add your name: ")
+    sport = input("Please add your favourite sports name: ")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
