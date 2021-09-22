53e475c876399a0d86b0b191b57dd4750154eddf
Adit14 <adit14121996@gmail.com>
2021-09-22 13:57:04 +0100

    Initail Commit

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your Full Name: ")
+    sport = input("Please add your favourite sport name: ")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
