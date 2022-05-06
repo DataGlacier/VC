d42ca5e8a2e464517f054b0a247ec9ecc3f615e9
Pranav120894 <102903318+Pranav120894@users.noreply.github.com>
2022-05-06 13:01:55 +0530

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -1,3 +1,15 @@
+{ 
+ "DG": "Test", 
+ -"Data Glacier": "Cricket", 
+-}
++ "Data Glacier": "Cricket",
++ "Pranav" : "Hockey"
++}
+
+
+
+
+
 #https: // github.com / DataGlacier / VC.git
 import json, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
