c717dfa9380abbdfd42f3ea33d42b3eddbb8dd05
mohinikal <amohini099@gmail.com>
2022-06-03 18:48:13 -0400

    entered name and favourite sport, complited task

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name: mohini")
+    sport = input("Please add your favourite sports name:basketball")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
