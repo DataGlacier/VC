99aa91708eab732a1d3359f7c3bbc344bc4c17d2
3000August <108506217+3000August@users.noreply.github.com>
2022-07-05 16:32:06 +0100

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name: Ogwu Augustine Ugbedeojo")
+    sport = input("Please add your favourite sports name: Football")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
