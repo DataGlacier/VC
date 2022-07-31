3088ad3651f35be1f59afb21bbf6f328dab8815d
Jason Robinson <robinsonjason761@gmail.com>
2022-07-30 22:12:30 -0500

    Inputting name and favorite sport

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name: Jason Robinson")
+    sport = input("Please add your favourite sports name: Football")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
