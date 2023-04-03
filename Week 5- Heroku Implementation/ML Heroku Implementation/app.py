c240696d4065ed15c844c554251e811e12835d06
Mahyar Shahpouri Arani <74094713+MahyarArani@users.noreply.github.com>
2023-04-03 11:55:31 -0400

    Week 5 Update

---
+++ Week 5- Heroku Implementation/ML Heroku Implementation/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+def manipulation(df):
+    df['State']= df['Customer Location'].str.split(" ").str[-2]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    # Get the input data from the HTML form
+    input_data = request.form.to_dict()
+    
+    # Preprocess the input data
+    input_df = pd.DataFrame(input_data, index=[0])
+    #input_df = manipulation(input_df)
+    input_df = input_df.drop(columns=[col for col in input_df.columns if col not in 
+                                       ['Price Of Sculpture', 'State', 'Artist Reputation',
+                                        'Base Shipping Price', 'Weight']])
+    
+    # Generate the prediction
+    prediction = model.predict(input_df)[0]
+    
+    # Render the HTML template with the prediction result
+    return render_template('templates/index.html', prediction_text='Shipping Cost Estimate is $ {}'.format(prediction))
+
+if __name__== '__main__':
+    app.run(debug = True)
