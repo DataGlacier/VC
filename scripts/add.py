58a87736009b797acdcd44786bda44fb5cb4d85a
Maria Contractor <112126933+mcont55@users.noreply.github.com>
2023-05-03 15:20:20 -0400

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name: Maria Contractor")
+    sport = input("Please add your favourite sports name: Tennis")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
