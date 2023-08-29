cdaa54e2283c63efb2572d4d5fa218cfee11b8e2
Kuzma12 <js940750@gmail.com>
2023-08-29 18:38:24 -0500

    code change

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_flask_code, request
+from flask import Flask, render_flask, request
 
 app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return render_flask_code("index.html")
+    return render_flask("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
