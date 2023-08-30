2526ed9859fc1b2b765daf0b6dafb7ffd53d42d1
Kuzma12 <js940750@gmail.com>
2023-08-29 20:05:33 -0500

    Fixing code
    Still in progress

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_flask, request
+from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+#import pandas as pd
 
-app = Flask('__name__')
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
 
-@app.route('/')
-def index():
-    return render_flask("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+
+@app.route("/")
+@app.route('/home', methods=['GET',"POST"])
+def home():
+    form = UploadFileForm()
+    return render_template('index.html', form=form)
+app.run(host="0.0.0.0", port =80)
+
+#Data
+#df_bonus = pd.read_csv('C:\Users\js940\Downloads\DataSets-main\City.csv')
+#df_bonus
+
+#data = pd.read_csv('C:\Users\js940\Downloads\DataSets-main\Customer_ID.csv')
+#data
+
+
+
+#from flask import Flask, render_flask, request
+#import flask
+
+#app = Flask('__name__')
+
+#@app.route('/')
+#def index():
+#    return render_flask("index.html")
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
