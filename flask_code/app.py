37f5f52a538931effcd76a7f9ed92fd29234c6d5
Kuzma12 <js940750@gmail.com>
2023-09-02 14:02:54 -0500

    New code
    Update the display

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,40 +1,36 @@
+import dataclasses
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
-#import pandas as pd
-
-class UploadFileForm(FlaskForm):
-    file = FileField("File")
-    submit = SubmitField("Upload File")
-
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
 
 @app.route("/")
-@app.route('/home', methods=['GET',"POST"])
 def home():
-    form = UploadFileForm()
-    return render_template('index.html', form=form)
-app.run(host="0.0.0.0", port =80)
+    Data = [
+        ("NEW YORK NY", 8405837, 302149),
+        ("CHICAGO IL", 1955130, 164468),
+        ("LOS ANGELES CA", 1595037, 144132),
+        ("MIAMI FL", 1339155, 17675),
+        ("SILICON VALLEY", 1177609, 27247),
+        ("ORANGE COUNTY", 1030185, 12994),
+        ("SAN DIEGO CA", 959307, 69995),
+        ("PHOENIX AZ", 943999, 6133),
+        ("DALLAS TX", 942908, 22157),
+        ("ATLANTA GA", 814885, 24701),
+        ("DENVER CO", 754233, 12421),
+        ("AUSTIN TX", 698371, 14978),
+        ("SEATTLE WA", 671238, 25063),
+        ("TUCSON AZ", 631442, 5712),
+        ("SAN FRANCISCO CA", 629591, 213609),
+        ("SACRAMENTO CA", 545776, 7044),
+        ("PITTSBURGH PA", 542085, 3643),
+        ("WASHINGTON DC", 418859, 127001),
+        ("NASHVILLE TN", 327225, 9270),
+        ("BOSTON MA", 248968, 80021),
+    ]
 
-#Data
-#df_bonus = pd.read_csv('C:\Users\js940\Downloads\DataSets-main\City.csv')
-#df_bonus
+    labels = [row[0] for row in Data]
+    values1 = [row[1] for row in Data]
+    values2 = [row[2] for row in Data]
 
-#data = pd.read_csv('C:\Users\js940\Downloads\DataSets-main\Customer_ID.csv')
-#data
+    return render_template("index.html", labels=labels, values1=values1, values2=values2)
 
-
-
-#from flask import Flask, render_flask, request
-#import flask
-
-#app = Flask('__name__')
-
-#@app.route('/')
-#def index():
-#    return render_flask("index.html")
-
-#if __name__ == '__main__':
-#    app.run(debug=True)
