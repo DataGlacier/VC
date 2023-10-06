6a9697eac3c7e76911ea9b4716a9fb1237f18211
JonathanCAllen <147160939+JonathanCAllen@users.noreply.github.com>
2023-10-06 13:49:27 +0100

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Jonathan Allen")
+    sport = input("Football")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
